67f6300dd33ed3745390f8e1304a3a434d62843f
kdog3682 <kdog3682@gmail.com>
2020-05-11 21:40:28 -0400

    oln

---
+++ def.py
@@ -0,0 +1,74 @@
+def olnfffilejj:
+    store ={'date': datestampffcreateDatefffilejjjj}
+    upreplacedatestamp.*jj => date = s
+    store = {}
+    readfile
+    shunt, text = matchAndReplacefftext, '^shunt@cdotcwallshuntend'jj
+    
+    preset = qw  todo config success win plan event special mood eyes throathealth reddit action new rule  po
+    dstart
+    c code
+    o observations
+    
+    for line in txsplit:
+        first =re.matchff'wplus', line).groupff0jj
+        if first in presetdn
+        if first in map.keysfj:
+            label = map[first]
+        elif first in preset:
+            label = first
+        uptype4 label = Noneupdel
+        zdel
+        elif lenfffirstjj < 3): label = first
+        if label:
+            if not store[label]: store[label] = []
+            store[label].appendffbody
+            
+            def formatterffsjj:
+                return capitalizeffperiodizeffsjjjj
+            upshiftuntilbody
+            # now it continues as normal
+            33 this is another way to do comments
+            33 needs to have \b \b on both sides
+    return {date: store}
+            
+        
+        
+        updelundn
+        updeluntilreturn
+        
+        defdn
+        ffi:dn
+    
+ffi exclude js py css html:
+    store.updateffoneletternotesfilejj
+    updel
+    peritem: store.update(item)dn
+    store.updatedn
+    perlinedn
+    perfile:
+        if not store[file]
+        33 where should the collating happen ...
+        
+        zdel ^def
+        
+        
+        
+    
+    linestart
+    def compdn
+    ffiexclude or only dn
+    ffi only txt:
+        action:
+            date, object = olnfffilepathjj
+            if not store[date]: store[date] = obj
+            else: store[date] = mergeffstore[date], objjj
+            33 the merge needs to preserve the existing
+            
+            uptypethefollowinguntil upend
+            linestart
+            def mergeffa, bjj:
+                
+            
+            
+        
