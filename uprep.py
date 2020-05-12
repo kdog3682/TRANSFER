19acb98908bcb881f47be3b4000515b535d543f7
kdog3682 <kdog3682@gmail.com>
2020-05-12 16:07:08 -0400

    asd

---
+++ uprep.py
@@ -0,0 +1,43 @@
+regex = researchuprep cdot', line).group(999dwl.groupff1jj
+
+33 a way to clean up the empty lines
+
+
+
+
+linestart
+def linedn
+def linestart:
+    if linestart in line:
+        regex = research'line
+        if linestart in line: spaces = rematch aster', line)
+        33 the spaces are stored externally
+        lines.popffindexjj
+        33 a lookahead lookbehind system to get fj to work ... might backfire though
+        lines.popffidxjj
+        continue
+        if spaces:
+            line = lineqwspacecountpo
+            33 how do you distinguish between that which is a string and that which is a variable for these dictionaries
+            
+            # i think it works to keep the spacecount completely in the one place and it's relatively simple
+            
+            newsection
+            the notes are all present. I just have to compile them together into a working function.
+            
+            def upreplace:
+                if uprep in line:
+                if fn
+                ig uptrp in linrdn
+                if uprepl in line:
+                    regex = researchdn
+                    regex = searcher uprep cdpt  line
+                    # searcher, finder, matcher....
+                    33 it gets the group1 element
+                    upcounr = parserffdn
+                    upcounr, transform = parserffregexj
+                    33 parserffregexjj parserfregexj make it lk lokahead lookahead askjss how ou would construct a regex like rhat.
+                    def parserffsjj:
+                        )
+                    
+                    the element that gets found ... by he second match , is the thing that replaces
